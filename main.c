399723057a92262cc2f640d059296701aa8651d6
kiranmuragodnavar <79253510+kiranmuragodnavar@users.noreply.github.com>
2022-09-07 00:49:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int who;
+	printf("1. for shopping, 2.for admin 3.exit");
+	scanf("%d",&who);
+	switch(who)
+	case 1:	;//shopper();
+	case2: ;//admin();
+	default: exit(0);
+	return 0;
+}
+	
+	 
---
+++ shopper.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void shopper ()
+{
+	int n;
+	char *name,*password;
+	printf("Enter 1:if you are already signed in user, Enter 2:if you want to sign in a new account,Enter 3:to exit");
+	scanf("%d",&n);
+	
